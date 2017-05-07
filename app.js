7eedcb8d041b6d7756ccaaccccc4673ee8aa18bb
Braden Lawrence <lawrencebradenm@gmail.com>
2017-05-07 18:35:09 +0000

    Adding index, new, and create routes and basic css layout
    - Trying out Semantic UI for nav bars and forms
    - Index page pulls blog entries from mongoose db
    - New has a form that creates a blogEntry object from title, image, and text inputs
    - Create route pulls that object using body-parser and uses it to create a new db entry, then redirects to index.

--- app.js
+++ app.js
@@ -18,9 +18,45 @@ var blogSchema = new mongoose.Schema({
 })
 var Blog = mongoose.model("Blog", blogSchema)
 
+// Blog.create({
+//     title: "Test post, please ignore.",
+//     text: "Nothing to see here. Keep moving, citizen.",
+//     image: "http://i.imgur.com/IbDSnAG.jpg"
+// })
+
 // Routes
+app.get("/", function(request, response){
+    response.redirect("/blogs")
+})
+
 app.get("/blogs", function(request, response){
-    response.send("Index route response.")
+    Blog.find({}, function(error, dbResponse){
+        if(error){
+            console.log("Could not retrieve blog entries.")
+        } else {
+            response.render("index", {blogs: dbResponse})        
+        }
+    })
+    
+})
+
+app.get("/blogs/new", function(request, response){
+    response.render("new")
+})
+
+app.post("/blogs", function(request, response){
+    var entry = {
+        title:  request.body.title,
+        text:   request.body.text,
+        image:  request.body.image
+    }
+    Blog.create(entry, function(error, dbResponse){
+        if(error){
+            console.log("Oh no!")
+        } else {
+            response.redirect("/blogs")
+        }
+    })
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
